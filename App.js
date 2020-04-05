4701baff82d2b40a079bd0e81aba22b6a4efb890
ToshihiroSawada <use.github.mail@gmail.com>
2020-04-05 23:47:10 +0900

    add Login Screen

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'; //react-navigation-stackのcreateStackNavigatorをインポート
+import { createAppContainer } from 'react-navigation'; //react-navigationのcreateAppContainerをインポート
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import LoginScreen from './src/screens/LoginScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppScreen = createStackNavigator({ //createStackNavigatorで画面を作成
+  Login: {
+    screen: LoginScreen,
+  },
+}, { //画面を用意する部分の外(以下の部分)にdefaultNavigationOptionsを出すことによって、全画面共通のオプションを作成できる
+  defaultNavigationOptions: {
+    headerTitle: '時間管理', //ヘッダータイトルを'MEMOT'に変更
+    headerTintColor: '#fff', //左上に表示される矢印の色を指定
+    headerStyle: {
+      backgroundColor: '#225566', //バックグラウンドカラーを設定
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
   },
 });
+
+const App = createAppContainer(AppScreen); //AppScreenをcreateAppConteinerに入れて、コンテナ化
+
+
+export default App;
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+// import Loading from '../elements/Loading';
+
+class LoginScreen extends React.Component {
+  state = { //テスト時に入力が面倒な場合、シングルクォートの中に記述してくと入力が不要になる
+    email: '',
+    password: '',
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          ログイン
+        </Text>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}/*この行がないと文字を入力しても反映されない */
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+          underlineColorAndroid="transparent" //AndroidのTextInputでに下線が出ないようにする設定(表示されなかったが一応入れておく)
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+          underlineColorAndroid="transparent" //AndroidのTextInputでに下線が出ないようにする設定(表示されなかったが一応入れておく)
+        />
+        <TouchableHighlight style={styles.button} underlayColor="#f7f">
+          <Text style={styles.buttonTitle}>ログインする</Text>
+        </TouchableHighlight>
+        {/*TouchableHighlightとTouchableOpacityの違いは細かい動作だけ大きくは変わらず、ただのボタン*/}
+        <TouchableOpacity style={styles.signup}>
+          <Text style={styles.signupText}>メンバー登録する</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#f0f',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center', //文字(送信)を中央に配置
+    width: '70%',
+    alignSelf: 'center', //ボタンの位置を中央に配置
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  signup: {
+    marginTop: 16,
+    alignSelf: 'center',
+  },
+  signupText: {
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
