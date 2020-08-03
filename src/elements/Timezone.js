7670cc8be106468905951f4d523d07d8584f4779
ToshihiroSawada <use.github.mail@gmail.com>
2020-08-04 00:19:56 +0900

    refact src/elements/Timezone.js

--- src/elements/Timezone.js
+++ src/elements/Timezone.js
@@ -5,8 +5,41 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class Timezone extends React.Component {
+  state = {
+    array: this.props.timeItemList,
+  }
+
+  //ArrayのstartTimeとendTimeが一致する場合の処理
+  viewPush(viewStack, key, array, i, j) {
+    viewStack.push(
+      <TouchableOpacity style={styles.timeView} onPress={() => { this.props.navigation.navigate('PlanEdit', array[j]); }}>
+        <Text style={styles.timeText} key={key}>{i}:00</Text>
+        <View style={[styles.plan, { backgroundColor: array[j].color }]}>
+          <Text style={styles.matterText}>{array[j].title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  //ArrayのstartTimeとendTimeが一致しない場合endTimeと一致するまでループ(Planの内容は同じ)
+  viewCreate(key, viewStack, array, i, j) {
+    // eslint-disable-next-line no-param-reassign
+    key += 1;
+    this.viewPush(viewStack, key, array, i, j);
+    if (array[j].startTime !== array[j].endTime) {
+      // eslint-disable-next-line no-param-reassign
+      i += 1;
+      // eslint-disable-next-line no-param-reassign
+      for (; i < array[j].endTime; i += 1) {
+        this.viewPush(viewStack, key, array, i, j);
+      }
+      this.viewPush(viewStack, key, array, i, j);
+    }
+    return [key, i, j];
+  }
+
   render() {
-    const array = this.props.timeItemList;
+    const { array } = this.state;
     //予定の個数を数えるカウンター
     const planCounter = array.length;
     let key = 0;
@@ -29,30 +62,9 @@ class Timezone extends React.Component {
         }
         //フラグが立った場合Viewをpushする
         if (planFlag === true) {
-          key += 1;
-          viewStack.push(
-            <TouchableOpacity style={styles.timeView} onPress={() => { this.props.navigation.navigate('PlanEdit', array[j]); }}>
-              <Text style={styles.timeText} key={key}>{i}:00</Text>
-              <View style={[styles.plan, { backgroundColor: array[j].color }]}>
-                <Text style={styles.matterText}>{array[j].title}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-          //上記でpushしたArrayのstartTimeとendTimeが一致しない場合endTimeと一致するまでループ(Planの内容は同じ)
-          if (array[j].startTime !== array[j].endTime) {
-            for (; i < array[j].endTime; i += 1) {
-              //key += 1;
-              viewStack.push(
-                <TouchableOpacity style={styles.timeView} onPress={() => { this.props.navigation.navigate('PlanEdit', array[j]); }}>
-                  <Text style={styles.timeText} key={key}>{i + 1}:00</Text>
-                  <View style={[styles.plan, { backgroundColor: array[j].color }]}>
-                    <Text style={styles.matterText}>〃</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }
-          }
+          [key, i, j] = this.viewCreate(key, viewStack, array, i, j);
         }
+        //フラグが立たなかった場合、時間のみ記述した空のViewをPushする
         else {
           key += 1;
           viewStack.push(
@@ -78,30 +90,9 @@ class Timezone extends React.Component {
         }
         //フラグが立った場合Viewをpushする
         if (planFlag === true) {
-          key += 1;
-          viewStack.push(
-            <TouchableOpacity style={styles.timeView} onPress={() => { this.props.navigation.navigate('PlanEdit', array[j]); }}>
-              <Text style={styles.timeText} key={key}>{i}:00</Text>
-              <View style={[styles.plan, { backgroundColor: array[j].color }]}>
-                <Text style={styles.matterText}>{array[j].title}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-          //上記でpushしたArrayのstartTimeとendTimeが一致しない場合endTimeと一致するまでループ(Planの内容は同じ)
-          if (array[j].startTime !== array[j].endTime) {
-            for (; i < array[j].endTime; i += 1) {
-              //key += 1;
-              viewStack.push(
-                <TouchableOpacity style={styles.timeView} onPress={() => { this.props.navigation.navigate('PlanEdit', array[j]); }}>
-                  <Text style={styles.timeText} key={key}>{i + 1}:00</Text>
-                  <View style={[styles.plan, { backgroundColor: array[j].color }]}>
-                    <Text style={styles.matterText}>〃</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }
-          }
+          [key, i, j] = this.viewCreate(key, viewStack, array, i, j);
         }
+        //フラグが立たなかった場合、時間のみ記述した空のViewをPushする
         else {
           key += 1;
           viewStack.push(
@@ -113,7 +104,6 @@ class Timezone extends React.Component {
         planFlag = false;
       }
     }
-    //console.log(viewStack);
     return (
       <View style={styles.container}>
         {viewStack}
